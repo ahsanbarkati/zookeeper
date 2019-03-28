969916a47a4931f25a54920c0892062ae9e0ab0a
Anant Prakash <anantprakashjsr@gmail.com>
2019-03-28 21:32:30 +0530

    Add script to start a swarm of 10 bimock

---
+++ startSwarm.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sh -c "./bin/bimock -p=9001 &\
+./bin/bimock -p=9002 &\
+./bin/bimock -p=9003 &\
+./bin/bimock -p=9004 &\
+./bin/bimock -p=9005 &\
+./bin/bimock -p=9006 &\
+./bin/bimock -p=9007 &\
+./bin/bimock -p=9009 &\
+./bin/bimock -p=9010 &\
+wait"
